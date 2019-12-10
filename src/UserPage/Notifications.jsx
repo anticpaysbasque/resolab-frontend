ac81d62f5cb950cd9cbdae45fe8f4d0db6e74122
angelique-w <angelique.wons@gmail.com>
2019-12-10 10:50:23 +0100

    modified notifications

--- src/UserPage/Notifications.jsx
+++ src/UserPage/Notifications.jsx
@@ -1,32 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import NotificationsIcon from "@material-ui/icons/Notifications";
+import Box from "@material-ui/core/Box";
+import axios from "axios";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles({
   card: {
-    maxWidth: 345
+    width: "1"
   }
-}));
+});
 
 export default function Notifications() {
   const classes = useStyles();
+
+  const [commentaires, setCommentaires] = useState([]);
+
+  const handleCommentsNotifications = () => {
+    axios
+      .get("")
+      .then(res => {
+        setCommentaires(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   return (
     <Card className={classes.card}>
       <CardHeader
         title={
-          <>
-            <NotificationsIcon />
-            <Typography>Notifications</Typography>
-          </>
+          <Box display="flex">
+            <NotificationsIcon style={{ padding: 10 }} />
+            <Typography style={{ padding: 10 }}>Notifications</Typography>
+          </Box>
         }
       ></CardHeader>
 
-      <CardContent></CardContent>
+      <CardContent>
+        {commentaires.map(commentaire => {
+          //return tous le nombre de commentaires pour chaque publication de la personne
+        })}
+      </CardContent>
     </Card>
   );
 }
--- src/UserPage/UserPage.jsx
+++ src/UserPage/UserPage.jsx
@@ -1,16 +1,30 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 
 import Layout from "../Layout/Layout";
 import PostArticle from "./PostArticle";
 import Card from "./Card";
 import Sidebar from "./Sidebar";
 
+const useStyles = makeStyles(theme => ({}));
+
 export default function UserPage() {
+  const classes = useStyles();
+
   return (
     <Layout>
-      <PostArticle />
-      <Card />
-      <Sidebar />
+      <Grid container spacing={3}>
+        <Grid container item xs={2}>
+          <PostArticle />
+        </Grid>
+        <Grid container item xs={6}>
+          <Card />
+        </Grid>
+        <Grid container item xs={4}>
+          <Sidebar />
+        </Grid>
+      </Grid>
     </Layout>
   );
 }
