c8dc19f10989c42d8fc9712a85ae00c8a8ac5bac
Clara Desperben <cdesperben@yahoo.fr>
2019-12-11 17:41:14 +0100

    fixed problems with headericons jsx files content

--- src/Layout/HeaderIcons.jsx
+++ src/Layout/HeaderIcons.jsx
@@ -2,8 +2,17 @@ import React from "react";
 import { Settings, ExitToApp } from "@material-ui/icons";
 import Grid from "@material-ui/core/Grid";
 import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
 
-function HeaderIcons() {
+import { removeToken } from "../reducers/actions";
+
+function HeaderIcons({ logOut }) {
+  const handleLogout = () => {
+    logOut();
+  };
+
+  let history = useHistory();
+
   return (
     <>
       <Grid
@@ -15,10 +24,16 @@ function HeaderIcons() {
       >
         <Settings color="default" />
 
-        <ExitToApp color="default" />
+        <ExitToApp color="default" onClick={handleLogout} />
       </Grid>
     </>
   );
 }
+
+const mapdispatchToProps = dispatch => {
+  return {
+    logOut: () => dispatch(removeToken())
+  };
+};
 
 export default connect(null, mapdispatchToProps)(HeaderIcons);
