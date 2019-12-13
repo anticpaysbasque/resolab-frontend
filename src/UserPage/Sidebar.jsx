4104fe39f1b01d0b1924b5fa12b1dbc59312c5b6
S LAVAUD <53374849+KleinosFR@users.noreply.github.com>
2019-12-13 10:14:08 +0100

    Update Sidebar.jsx

--- src/UserPage/Sidebar.jsx
+++ src/UserPage/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-//import Notifications from "./Notifications";
+import Notifications from "./Notifications";
 
 export default function Sidebar() {
-  return <div>{/* <Notifications /> */}</div>;
+  return <div> <Notifications /> </div>;
 }
