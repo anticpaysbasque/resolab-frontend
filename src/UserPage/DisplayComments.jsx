da92cf22864aa187e007fcad06287a9e74284630
angelique-w <angelique.wons@gmail.com>
2020-01-06 11:57:18 +0100

    display comments ok but not users who comments

--- src/UserPage/DisplayComments.jsx
+++ src/UserPage/DisplayComments.jsx
@@ -5,7 +5,7 @@ import { CardContent, Typography } from "@material-ui/core";
 import apiCallAuth from "../apiCallAuth";
 
 function DisplayComments({ comments }) {
-  const [contentComments, setContentComments] = useState("");
+  // const [contentComments, setContentComments] = useState("");
 
   // useEffect(() => {
   //     const fetchComments = async () => {
@@ -17,30 +17,36 @@ function DisplayComments({ comments }) {
 
   return (
     <CardContent>
-      {comments.map(comment => {
-        let content = "totojhj";
-        const fetchComments = async comment => {
-          const res = await apiCallAuth.get(`${comment}`);
-          setContentComments(res.data.content);
-          // console.log(content);
-        };
-
-        fetchComments(comment);
-
-        return <Typography>{contentComments}</Typography>;
-
-        // apiCallAuth
-        //     .get(`${comment}`)
-        //     .then(res => {
-        //         const datas = res.data.content;
-
-        //         content = datas;
-        //         console.log(content);
-        //         return <Typography>{content}</Typography>;
-        //     })
-        //     .catch(err => console.log(err));
-      })}
+      {comments.map(comment => (
+        <Typography>{comment.content}</Typography>
+      ))}
     </CardContent>
+
+    // <CardContent>
+    //   {comments.map(comment => {
+    //     let content = "totojhj";
+    //     const fetchComments = async comment => {
+    //       const res = await apiCallAuth.get(`${comment}`);
+    //       setContentComments(res.data.content);
+    //       // console.log(content);
+    //     };
+
+    //     fetchComments(comment);
+
+    //     return <Typography>{contentComments}</Typography>;
+
+    //     // apiCallAuth
+    //     //     .get(`${comment}`)
+    //     //     .then(res => {
+    //     //         const datas = res.data.content;
+
+    //     //         content = datas;
+    //     //         console.log(content);
+    //     //         return <Typography>{content}</Typography>;
+    //     //     })
+    //     //     .catch(err => console.log(err));
+    //   })}
+    // </CardContent>
   );
 }
 
