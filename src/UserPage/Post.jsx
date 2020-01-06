b547f59743b78567eb2854daa5ffd1c5176ff6be
Clara Desperben <cdesperben@yahoo.fr>
2020-01-06 16:57:21 +0100

    adding ike is effective,  but deletion causes issues about render

--- src/UserPage/Post.jsx
+++ src/UserPage/Post.jsx
@@ -24,7 +24,7 @@ import { connect } from "react-redux";
 
 import apiCallAuth from "../apiCallAuth";
 import CommentInput from "./CommentInput";
-import Axios from "axios";
+import axios from "axios";
 
 function Post({
   description,
@@ -35,27 +35,25 @@ function Post({
   userId,
   comments,
   likes,
-  owner
+  owner,
+  token
 }) {
   const [inputCommentPost, setInputComment] = useState(false);
   const [inputValue, setInputValue] = useState("");
   const [isInputEmpty, setIsInputEmpty] = useState(false);
-  const [likesCount, setLikesCount] = useState(likes.length);
-
   const [isLiked, setIsLiked] = useState(false);
-
   const [alert, setAlert] = useState(false);
-  // const [count, setCount] = useState(0);
-  const [timeOut, setTimeOut] = useState();
-
-  const [postOwnerInfo, setPostOwnerInfo] = useState({});
+  const [stateLikes, setStateLikes] = useState(likes);
+  const [likesCount, setLikesCount] = useState(stateLikes.length);
 
   useEffect(() => {
-    likes.map(like => {
-      if (like.user.id === userId) {
-        setIsLiked(true);
-      }
-    });
+    // likes.map(like => {
+    //     if (like.user.id === userId) {
+    //         setIsLiked(true);
+    //     }
+    // });
+
+    stateLikes.some(like => like.user.id === userId) && setIsLiked(true);
   }, []);
 
   const handlePostComment = () => {
@@ -86,15 +84,58 @@ function Post({
 
   const handleLike = () => {
     // Si c'est déjà liké, on supprime le like dans l'API, puis isLiked -> false, count -1
+    const config = {
+      headers: {
+        Authorization: "Bearer " + token
+      }
+    };
+    if (isLiked) {
+      const pireExempleDuMonde = stateLikes.find(like => {
+        if (typeof like.user === "string") {
+          return like.user.split("/")[3] === userId;
+        } else {
+          return userId === like.user.id;
+        }
+      });
+      if (pireExempleDuMonde) {
+        axios
+          .delete(
+            `http://localhost:8089/api/likes/${pireExempleDuMonde.id}`,
+            config
+          )
+          .then(() =>
+            axios.get(`http://localhost:8089/api/posts/${postId}/likes`, config)
+          )
+          .then(res => {
+            setStateLikes(res.data);
+            setIsLiked(false);
+            setLikesCount(res.data.length);
+          })
+          .catch(err => {
+            console.log(err.message);
+            throw err;
+          });
+      } else {
+        alert("Undefined !");
+      }
+    }
 
     // Si c'est pas liké, on crée le like dans l'API, puis isLiked -> true, count +1
     if (!isLiked) {
-      apiCallAuth
-        .post("/likes", {
-          user: `api/users/${userId}`,
-          post: `api/posts/${postId}`
-        })
+      axios
+        .post(
+          "http://localhost:8089/api/likes",
+          {
+            user: `api/users/${userId}`,
+            post: `api/posts/${postId}`
+          },
+          config
+        )
+        .then(() =>
+          axios.get(`http://localhost:8089/api/posts/${postId}/likes`, config)
+        )
         .then(res => {
+          setStateLikes(res.data);
           setIsLiked(true);
           setLikesCount(likesCount + 1);
         })
@@ -104,6 +145,7 @@ function Post({
         });
     }
   };
+
   const handleClickAlert = () => {
     setAlert(!alert);
   };
@@ -178,7 +220,8 @@ function Post({
 
 const mapStateToProps = state => {
   return {
-    userId: state.userReducer.id
+    userId: state.userReducer.id,
+    token: state.authReducer.token
   };
 };
 
