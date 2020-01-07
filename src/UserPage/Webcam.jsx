abe1baac5077b6c727a10a3c0fcf269b1c965b18
Clara Desperben <cdesperben@yahoo.fr>
2020-01-07 14:52:01 +0100

    created a new webcam component to upload images from the webcam, created some features into it such as webcam constraints for thee format

---
+++ src/UserPage/Webcam.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Webcam from "react-webcam";
+
+const webcamComponent = () => <Webcam />;
+
+const rulesConstraint = {
+  // using a square format for the picture to remind the polaroid style used on Instagram
+  width: 720,
+  height: 720,
+  facingMode: user
+};
+
+function webcamComponent() {
+  return {};
+}
+
+export default webcamComponent;
