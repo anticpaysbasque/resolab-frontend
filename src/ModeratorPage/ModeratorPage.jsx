9fe157d5c723f0d6c21face1d389fa542740e3c9
Clara Desperben <cdesperben@yahoo.fr>
2020-01-12 18:46:55 +0100

    moderator page is operational, it has almost the same components as userpage ; maybe we will see in the future modifications about informations on the components

--- src/ModeratorPage/ModeratorPage.jsx
+++ src/ModeratorPage/ModeratorPage.jsx
@@ -1,5 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, Grid, Snackbar } from "@material-ui/core";
+
+import { useStyles } from "../UserPage/useStyles";
+import PostStorie from "../UserPage/PostStorie";
+import Layout from "../Layout/Layout";
+import DisplayStories from "../UserPage/DisplayStories";
+import PostArticle from "../UserPage/PostArticle";
+import Publications from "../UserPage/Publications";
+import Notifications from "../UserPage/Notifications";
+import ModeratorInfo from "./ModeratorInfos";
 
 export default function ModeratorPage() {
-  return <div>'Hello from Moderator Page!'</div>;
+  const classes = useStyles();
+  const [snackBarNotification, setSnackBarNotification] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+
+  const handleSnackBar = message => {
+    setSnackBarNotification(true);
+    setSnackbarMessage(message);
+  };
+  return (
+    <Layout>
+      <Box mt={15}>
+        <Grid container spacing={3} direction="row" alignItems="center">
+          <Grid container item xs={2} justify="center">
+            <PostStorie classes={classes} handleSnackBar={handleSnackBar} />
+          </Grid>
+          <Grid container item xs={9} justify="center">
+            <DisplayStories classes={classes} />
+          </Grid>
+        </Grid>
+      </Box>
+      <Grid container spacing={3}>
+        <Grid container item xs={2} justify="center">
+          <PostArticle handleSnackBar={handleSnackBar} />
+        </Grid>
+        <Grid container item xs={6} xl={6} justify="center">
+          <Publications handleSnackBar={handleSnackBar} />
+        </Grid>
+        <Grid container item xs={3} xl={3} justify="center">
+          <Notifications>
+            <ModeratorInfo />
+          </Notifications>
+        </Grid>
+      </Grid>
+      <Snackbar
+        open={snackBarNotification}
+        setOpen={setSnackBarNotification}
+        handleSnackBar={handleSnackBar}
+        message={snackbarMessage}
+      />
+    </Layout>
+  );
 }
