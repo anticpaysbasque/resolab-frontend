c69f183f764d9eec95762233a73a131c0a28faf0
angelique-w <angelique.wons@gmail.com>
2020-01-15 17:58:07 +0100

    merged discusscard component card to expansion panel

---
+++ src/commonComponent/DiscussContacts.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  Box,
+  Grid,
+  Typography,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails
+} from "@material-ui/core";
+import ForumIcon from "@material-ui/icons/Forum";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+import ContactsList from "./ContactsList";
+
+function Contacts({ classes }) {
+  return (
+    <ExpansionPanel className={classes.sidebarCard}>
+      <ExpansionPanelSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+        className={classes.sidebarCardHeader}
+      >
+        <ForumIcon className={classes.sidebarCardHeaderElements} />
+        <Typography className={classes.sidebarCardHeaderElements}>
+          Discussion instantanée
+        </Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <Box
+          style={{
+            display: "flex",
+            alignItems: "center",
+            height: "250px",
+            overflowY: "auto",
+            overflowX: "visible",
+            paddingTop: "25px",
+            width: "100%"
+          }}
+        >
+          <Grid container direction="column" wrap="nowrap">
+            <ContactsList classes={classes} />
+          </Grid>
+        </Box>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+  );
+}
+
+export default Contacts;
--- src/commonComponent/Sidebar.jsx
+++ src/commonComponent/Sidebar.jsx
@@ -3,14 +3,14 @@ import React from "react";
 import "../Layout/Scroll.css";
 import UserInfo from "../UserPage/UserInfo";
 import Notifications from "./Notifications";
-import Contacts from "./Contacts";
+import DiscussContacts from "./DiscussContacts";
 
 export default function Sidebar({ classes }) {
   return (
-    <div style={{ position: "fixed", top: "120px" }}>
+    <div style={{ position: "fixed", top: "300px" }}>
       <UserInfo classes={classes} />
       <Notifications classes={classes} />
-      <Contacts classes={classes} />
+      <DiscussContacts classes={classes} />
     </div>
   );
 }
