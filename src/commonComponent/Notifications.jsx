5ee8a47cce0fdc7a54b7482f4f13a1e0240bf645
angelique-w <angelique.wons@gmail.com>
2020-01-15 17:40:19 +0100

    merged notifications card to expansion panel

--- src/commonComponent/Notifications.jsx
+++ src/commonComponent/Notifications.jsx
@@ -1,41 +1,48 @@
 import React from "react";
-import { Grid, Card, CardHeader, Typography } from "@material-ui/core";
-import { Notifications } from "@material-ui/icons/";
-import Box from "@material-ui/core/Box";
+import {
+  Typography,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+  Box,
+  Grid
+} from "@material-ui/core";
+import { Notifications, ExpandMore } from "@material-ui/icons/";
 
 import NotifyComments from "./NotifyComments";
 import NotifyLikes from "./NotifyLikes";
 
 export default function Notification({ classes }) {
   return (
-    <Card className={classes.sidebarCard}>
-      <CardHeader
-        title={
-          <Box display="flex">
-            <Notifications className={classes.sidebarCardHeaderElements} />
-            <Typography className={classes.sidebarCardHeaderElements}>
-              Notifications
-            </Typography>
-          </Box>
-        }
+    <ExpansionPanel className={classes.sidebarCard}>
+      <ExpansionPanelSummary
+        expandIcon={<ExpandMore />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
         className={classes.sidebarCardHeader}
-      ></CardHeader>
-
-      <Box
-        style={{
-          display: "flex",
-          alignItems: "center",
-          height: "250px",
-          overflowY: "auto",
-          overflowX: "visible",
-          paddingTop: "25px"
-        }}
       >
-        <Grid container direction="column" alignItems="center" wrap="nowrap">
-          <NotifyComments />
-          <NotifyLikes />
-        </Grid>
-      </Box>
-    </Card>
+        <Notifications className={classes.sidebarCardHeaderElements} />
+        <Typography className={classes.sidebarCardHeaderElements}>
+          Notifications
+        </Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <Box
+          style={{
+            display: "flex",
+            alignItems: "center",
+            height: "250px",
+            overflowY: "auto",
+            overflowX: "visible",
+            paddingTop: "25px"
+          }}
+        >
+          <Grid container direction="column" alignItems="center" wrap="nowrap">
+            <NotifyComments />
+            <NotifyLikes />
+          </Grid>
+        </Box>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
   );
 }
