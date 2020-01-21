875ea47408d9505ff7dd07ffc9df076afd3c919b
Karnak19 <basile64.v@gmail.com>
2020-01-21 12:14:33 +0100

    starting created useTimer hook

--- src/hooks/useTimer.js
+++ src/hooks/useTimer.js
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import moment from "moment";
 
-export function useTimer(start = new Date()) {
-  const [startTime, setStartTime] = useState(start);
+export function useTimer() {
+  const [startTime, setStartTime] = useState(null);
+  const [duration, setDuration] = useState(null);
+
+  const getTime = () => {
+    setDuration((new Date() - startTime) / 1000);
+
+    setTimeout(() => {
+      getTime();
+    }, 1000);
+  };
+
+  useEffect(() => {
+    sessionStorage.getItem("start")
+      ? setStartTime(new Date(sessionStorage.getItem("start")))
+      : sessionStorage.setItem("start", new Date());
+    getTime();
+  }, []);
 
   return {
-    startTime
+    duration: Math.floor(duration)
   };
 }
