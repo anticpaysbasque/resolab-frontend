4bc74fdc421fb6d164b9422c6a808b1643e56b11
angelique-w <angelique.wons@gmail.com>
2020-01-23 09:59:12 +0100

    ordered alerts by TakenCare key

--- src/ModeratorPage/AlertsList.jsx
+++ src/ModeratorPage/AlertsList.jsx
@@ -1,28 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { List } from "@material-ui/core";
+import { orderBy } from "lodash";
 
 import Alert from "./Alert";
 import { useRecursiveGet } from "../hooks/useApi";
 
 function AlertsList({ classes, setAlertCount }) {
   const { datas, request } = useRecursiveGet("/alerts", 10000);
+  const [orderedAlertsByTakenInCharge, setOrderedAlerts] = useState([]);
 
   useEffect(() => {
     request();
+    const orderedAlerts = datas && orderBy(datas, ["takenCare"], ["asc"]);
+    setOrderedAlerts(orderedAlerts);
   }, []);
 
   useEffect(() => {
     datas && setAlertCount(datas.length);
+    const orderedAlerts = datas && orderBy(datas, ["takenCare"], ["asc"]);
+    setOrderedAlerts(orderedAlerts);
   }, [datas]);
 
   return (
     <List>
-      {datas &&
-        datas
-          .reverse()
-          .map(alert => (
-            <>{!alert.resolved && <Alert alert={alert} classes={classes} />}</>
-          ))}
+      {orderedAlertsByTakenInCharge &&
+        orderedAlertsByTakenInCharge.map(alert => (
+          <>{!alert.resolved && <Alert alert={alert} classes={classes} />}</>
+        ))}
     </List>
   );
 }
