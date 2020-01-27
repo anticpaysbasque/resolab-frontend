ad7fdc5d0e90c98df44a26f46f2e5e0a17cf36f6
angelique-w <angelique.wons@gmail.com>
2020-01-28 00:27:01 +0100

    added context of comments that is alerted

--- src/ModeratorPage/ModerationContent.jsx
+++ src/ModeratorPage/ModerationContent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   Typography,
   CardMedia,
@@ -11,24 +11,11 @@ import {
   Divider
 } from "@material-ui/core";
 import { PermIdentity, ChatBubbleOutline } from "@material-ui/icons";
-import axios from "axios";
 import { connect } from "react-redux";
 
 import DisplayCommentsPostsAlerts from "./DisplayCommentsPostsAlerts";
 
-const apiUrl = process.env.REACT_APP_MEDIA_URL;
-
 function ModerationContent({ classes, openAlert, token }) {
-  const config = {
-    headers: {
-      Authorization: "Bearer " + token,
-      Accept: "application/json"
-    }
-  };
-
-  const [post, setPost] = useState({});
-  const [comments, setComments] = useState([]);
-
   if (openAlert.post) {
     return (
       <>
@@ -46,70 +33,13 @@ function ModerationContent({ classes, openAlert, token }) {
     );
   }
   if (openAlert.comment) {
-    console.log("coucou");
-    let datapost;
-    // let post;
-    // let comments;
-    axios
-      .get(`${apiUrl}/api/comments/${openAlert.comment.id}`, config)
-      .then(res => {
-        datapost = res.data.post;
-      })
-      .then(() => {
-        axios
-          .get(`${apiUrl}${datapost}`, config)
-          .then(res => {
-            setPost(res.data);
-            // post = res.data;
-          })
-          .catch(err => {
-            console.log(err.message);
-            throw err;
-          });
-      })
-      .then(() => {
-        // comments = post.comments;
-        setComments(post.comments);
-      })
-      .catch(err => {
-        console.log(err.message);
-        throw err;
-      });
-
     return (
-      <>
-        <Typography>
-          {openAlert.user.username} a lancé une alerte sur le commentaire de{" "}
-          {openAlert.comment.user.username} :
-        </Typography>
-        <Typography>"{openAlert.comment.content}"</Typography>
-        <Typography>{openAlert.comment.createdAt}</Typography>
-        <Divider />
-        <Typography>
-          Contexte dans lequel ce commentaire a été émis :
-        </Typography>
-        <Card style={{ width: "100%" }}>
-          <CardHeader
-            avatar={
-              <Avatar aria-label="recipe" className={classes.avatar}>
-                <PermIdentity />
-              </Avatar>
-            }
-            title={
-              <Typography className={classes.username}>
-                {openAlert.comment.user.username}
-              </Typography>
-            }
-          />
-          <CardMedia className={classes.media} image={post.photo} />
-          <CardContent>
-            <Typography>{post.description}</Typography>
-          </CardContent>
-          <Typography>Commentaires :</Typography>
-          <DisplayCommentsPostsAlerts comments={comments} classes={classes} />
-        </Card>
-        <Divider />
-      </>
+      <DisplayCommentsPostsAlerts
+        openAlert={openAlert}
+        comment={openAlert.comment}
+        classes={classes}
+        token={token}
+      />
     );
   }
   if (openAlert.story) {
